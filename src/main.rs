00ce56217ba0ceafe476c16db4f93cd1d560818a
kraxarn <me@kraxarn.com>
2020-10-24 14:31:34 +0200

    Initial logger

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,17 @@ mod ui;
 
 pub const APPLICATION_ICON: Option<&str> = Some("go-home");
 
+fn init_logger() -> Result<(), simplelog::TermLogError> {
+	simplelog::TermLogger::init(
+		simplelog::LevelFilter::max(),
+		simplelog::Config::default(),
+		simplelog::TerminalMode::Mixed,
+	)
+}
+
 fn main() {
+	init_logger().expect("failed to init logger");
+
 	let app = gtk::Application::new(Some("com.crow.gfr"), Default::default())
 		.expect("failed to init gtk application");
 	app.connect_activate(|app| {
